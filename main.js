05609c3cb1aeb65b7c6715cbb3fc7510f0ca5d7d
Chris-Fitzpatrick <chrishfitzpatrick@gmail.com>
2016-10-15 00:37:21 -0400

    nothing much yet

---
+++ main.js
@@ -0,0 +1,30 @@
+var imageArray = new Array();
+imageArray[0] = new Image();
+imageArray[0].src = 'images/beautiful-mountains.jpg';
+imageArray[1] = new Image();
+imageArray[1].src = 'images/beautiful-beach.jpg';
+imageArray[2] = new Image();
+imageArray[2].src = 'images/beautiful-forest-filled-with-trees.jpg';
+imageArray[3] = new Image();
+imageArray[3].src = 'images/beautiful-hillside.jpg';
+imageArray[4] = new Image();
+imageArray[4].src = 'images/beautiful-mountains-with-river.jpg';
+imageArray[5] = new Image();
+imageArray[5].src = 'images/beautiful-ocean-view.jpg';
+imageArray[6] = new Image();
+imageArray[6].src = 'images/beautiful-rolling-hills.jpg';
+imageArray[7] = new Image();
+imageArray[7].src = 'images/beautiful-tree-and-sunset.jpg';
+imageArray[8] = new Image();
+imageArray[8].src = 'images/beautiful-yet-stark-mountain.jpg';
+
+function getImage(){
+        var i = Math.random() * 8;
+        //background image = imageArray[i];
+}
+
+var quoteArray = new Array();
+quoteArray[0] = "Things aren't looking good today";
+function getQuote(){
+
+}
