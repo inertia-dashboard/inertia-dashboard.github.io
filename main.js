8e0db27051aaa11cb01583b03b002134e3257320
Chris-Fitzpatrick <chrishfitzpatrick@gmail.com>
2016-10-15 01:08:36 -0400

    added js file and updated image names

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 var imageArray = new Array();
 imageArray[0] = new Image();
-imageArray[0].src = 'images/beautiful-mountains.jpg';
+imageArray[0].src = 'images/beautifulMountains.jpg';
 imageArray[1] = new Image();
-imageArray[1].src = 'images/beautiful-beach.jpg';
+imageArray[1].src = 'images/beautifulBeach.jpg';
 imageArray[2] = new Image();
-imageArray[2].src = 'images/beautiful-forest-filled-with-trees.jpg';
+imageArray[2].src = 'images/beautifulForestFilledWithTrees.jpg';
 imageArray[3] = new Image();
-imageArray[3].src = 'images/beautiful-hillside.jpg';
+imageArray[3].src = 'images/beautifulHillside.jpg';
 imageArray[4] = new Image();
-imageArray[4].src = 'images/beautiful-mountains-with-river.jpg';
+imageArray[4].src = 'images/beautifulMountainsWithRiver.jpg';
 imageArray[5] = new Image();
-imageArray[5].src = 'images/beautiful-ocean-view.jpg';
+imageArray[5].src = 'images/beautifulOceanView.jpg';
 imageArray[6] = new Image();
-imageArray[6].src = 'images/beautiful-rolling-hills.jpg';
+imageArray[6].src = 'images/beautifulRollingHills.jpg';
 imageArray[7] = new Image();
-imageArray[7].src = 'images/beautiful-tree-and-sunset.jpg';
+imageArray[7].src = 'images/beautifulTreeAndSunset.jpg';
 imageArray[8] = new Image();
-imageArray[8].src = 'images/beautiful-yet-stark-mountain.jpg';
+imageArray[8].src = 'images/beautifulYetStarkMountain.jpg';
+
+function init(){
+        getImage();
+}
 
 function getImage(){
         var i = Math.random() * 8;
+        console.log(i);
         //background image = imageArray[i];
+        var background = new Image();
+        background = imageArray[i];
+        console.log(background);
+        document.body.style.backgroundImage = background;
 }
 
 var quoteArray = new Array();
