ec626c2f7c71fd69461ae32c42989ca9cab2ebfa
Josh Berl <Josh@Joshuas-MacBook-Air-4.local>
2016-10-15 10:16:09 -0400

    moved most js to main

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var request = new XMLHttpRequest();
+var mylat = 0;
+var mylong = 0;
+var me = new google.maps.LatLng(mylat, mylong);
+
 var imageArray = new Array();
 imageArray[0] = new Image();
 imageArray[0].src = 'images/beautifulMountains.jpg';
@@ -22,6 +27,7 @@ function init() {
         getImage();
         getQuote();
         getLiveTime();
+        initialize();
 }
 
 function getImage(){
@@ -36,13 +42,19 @@ var quoteArray = new Array();
 quoteArray[0] = "things aren't looking good today.";
 quoteArray[1] = "everybody you know is going to die.";
 quoteArray[2] = "tech is a bubble.";
+quoteArray[3] = "tons of people die for no reason, that's what makes death so terrifying";
+quoteArray[4] = "death is inevitable";
+quoteArray[5] = "fame is a vapor, the only certainty in life is oblivion";
+quoteArray[6] = "some fish spend their whole lives swimming upstream only to be eaten by a bear";
+quoteArray[7] = "you can't make toast back into bread";
+quoteArray[8] = "i think we are just insects, we live a bit and then die and that’s the lot. there’s no mercy in things. there’s not even a great beyond. there’s nothing";
+quoteArray[9] = "this is not going to work";
 
 function getQuote(){
-	var i = Math.floor(Math.random() * 3);
+	var i = Math.floor(Math.random() * 9);
 	var element = document.getElementById("quotebox");
 	console.log(quoteArray[i]);
-	element.innerHTML = quoteArray[i];
-	//$("quotebox").prepend("<p>" + quoteArray[i] + "<p>");
+	element.innerHTML = "\"" + quoteArray[i] + "\"";
 }
 
 function getLiveTime() {
@@ -64,6 +76,63 @@ function checkTime(i) {
 }
 
 
+function initialize(){
+        console.log("init starting");
+        getMyLocation();    
+}
+
+
+function getMyLocation(){
+
+    console.log("starting getmyloc");
+    if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position){
+        console.log("got position");
+        getWeather(position.coords.latitude, position.coords.longitude);
+}
+
+
+var getWeather = function(mylat, mylong) {
+    gettingData = true;
+    var requestString = "http://api.openweathermap.org/data/2.5/weather?lat="
+                        + mylat + "&lon=" + mylong  
+                        + "&APPID=68857f90d32a5dd13ead22ecc79f8af8";
+    request = new XMLHttpRequest();
+    request.onload = proccessResults;
+    request.open("get", requestString, true);
+    request.send();
+
+        console.log("lat is " + mylat);
+        console.log("long is " + mylong);
+};
+
+var proccessResults = function() {
+    console.log(this);
+    var results = JSON.parse(this.responseText);
+    var temperature = results.main.temp; /* units are in kelvin lol */
+    var element = document.getElementById("temperaturebox");
+    temperature = (Math.floor(1.8 * (temperature - 273) +32)); 
+    temperature = parseInt(temperature);
+    if (temperature < 0) weatherText = "it's " + temperature + " degrees outside. you should move.";
+    else if (temperature >= 0 && temperature < 30) weatherText = "it's " + temperature + " degrees outside. you should probably just stay inside";
+    else if (temperature >= 30 && temperature < 40) weatherText = "it's " + temperature + " degrees outside wear a big jacket or something";
+    else if (temperature >= 40 && temperature < 50) weatherText = "it's " + temperature + " degrees outside wear a  jacket or something";
+    else if (temperature >= 50 && temperature < 60) weatherText = "it's " + temperature + " degrees outside wear a light jacket or sweatshirt or something";
+    else if (temperature >= 60 && temperature < 70) weatherText = "it's " + temperature + " degrees outside wear like jeans and a t shirt or something like that";
+    else if (temperature >= 70 && temperature < 80) weatherText = "it's " + temperature + " degrees outside wear a t shirt and pants or shorts or something. who am i to say?";
+    else if (temperature >= 80 && temperature < 90) weatherText = "it's " + temperature + " degrees outside wear shorts or something";
+    else if (temperature >= 90 && temperature < 100) weatherText = "it's " + temperature + " degrees outside so you should probably stay inside";
+    else if (temperature >= 100) weatherText = "it's " + temperature + " degrees outside. you should move.";
+    else weatherText = "i don't know what it is like outside, look out a window or something.";
+    element.innerHTML = weatherText;
+
+};
 
 
 
--- temp.js
+++ temp.js
@@ -1,50 +1,86 @@
-var request = new XMLHttpRequest();
-var mylat = 0;
-var mylong = 0;
-var me = new google.maps.LatLng(mylat, mylong);
+// var request = new XMLHttpRequest();
+// var mylat = 0;
+// var mylong = 0;
+// var me = new google.maps.LatLng(mylat, mylong);
 
-function initialize() {
-        console.log("init starting");
-        getMyLocation();    
-}
-
-
-function getMyLocation(){
-
-    console.log("starting getmyloc");
-    if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position){
-        console.log("got position");
-        getWeather(position.coords.latitude, position.coords.longitude);
-}
+// function initialize(){
+//         console.log("init starting");
+//         getMyLocation();    
+// }
 
 
-var getWeather = function(mylat, mylong) {
-    gettingData = true;
-    var requestString = "http://api.openweathermap.org/data/2.5/weather?lat="
-                        + mylat + "&lon=" + mylong  
-                        + "&APPID=68857f90d32a5dd13ead22ecc79f8af8";
-    request = new XMLHttpRequest();
-    request.onload = proccessResults;
-    request.open("get", requestString, true);
-    request.send();
+// function getMyLocation(){
 
-        console.log("lat is " + mylat);
-        console.log("long is " + mylong);
-};
+//     console.log("starting getmyloc");
+//     if (navigator.geolocation) {
+//          navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
 
-var proccessResults = function() {
-    console.log(this);
-    var results = JSON.parse(this.responseText);
-    var temperature = results.main.temp; /* units are in kelvin lol */
-    temperature = (Math.floor(1.8 * (temperature - 273) +32)); 
-    var element = document.getElementById("temperature");
-    element.innerHTML = temperature;
+// function showPosition(position){
+//         console.log("got position");
+//         getWeather(position.coords.latitude, position.coords.longitude);
+// }
 
-  };
+
+// var getWeather = function(mylat, mylong) {
+//     gettingData = true;
+//     var requestString = "http://api.openweathermap.org/data/2.5/weather?lat="
+//                         + mylat + "&lon=" + mylong  
+//                         + "&APPID=68857f90d32a5dd13ead22ecc79f8af8";
+//     request = new XMLHttpRequest();
+//     request.onload = proccessResults;
+//     request.open("get", requestString, true);
+//     request.send();
+
+//         console.log("lat is " + mylat);
+//         console.log("long is " + mylong);
+// };
+
+// var proccessResults = function() {
+//     console.log(this);
+//     var results = JSON.parse(this.responseText);
+//     var temperature = results.main.temp; /* units are in kelvin lol */
+//     temperature = (Math.floor(1.8 * (temperature - 273) +32)); 
+//     console.log("about to do switch");
+//     switch (temperature) {
+//         case (temperature < 0):
+//             weatherText = "it's " + temperature + " degrees outside. you should move.";
+//             break;
+//         case (temperature <= 30 && temperature > 0):
+//             weatherText = "it's " + temperature + " degrees outside. you should probably just stay inside";
+//             break;
+//         case (temperature > 30 && temperature <= 40):
+//             weatherText = "it's " + temperature + " degrees outside wear a big jacket or something";
+//             break;
+//         case (temperature > 40 && temperature <= 50):
+//             weatherText = "it's " + temperature + " degrees outside wear a jacket or something";
+//             break;
+//         case (temperature > 50 && temperature <= 60):
+//             weatherText = "it's " + temperature + " degrees outside wear a light jacket or sweatshirt or something";
+//             break;
+//         case (temperature > 60 && temperature <= 70):
+//             weatherText = "it's " + temperature + " degrees outside wear like jeans and a t shirt or something like that";
+//             break;
+//         case (temperature > 70 && temperature <= 80):
+//             weatherText = "it's " + temperature + " degrees outside wear a t shirt and pants or shorts or something. who am i to say?";
+//             break;
+//         case (temperature > 80 && temperature <= 90):
+//             weatherText = "it's " + temperature + " degrees outside wear shorts or something";
+//             break;
+//         case (temperature > 90 && temperature <= 100):
+//             weatherText = "it's " + temperature + " degrees outside so you should probably stay inside";
+//             break;
+//         case (temperature > 100):
+//             weatherText = "it's " + temperature + " degrees outside. you should move.";     
+//             break;                    
+//         default: 
+//             weatherText = "i don't know what it is like outside, look out a window or something.";
+//     }
+//     console.log("end of switch");
+//     var element = document.getElementById("temperature");
+//     element.innerHTML = temperature;
+
+//   };
