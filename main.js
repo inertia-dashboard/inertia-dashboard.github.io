76d29f88fc4a9cfbc8deffa43bf6cfffb466da4a
Chris-Fitzpatrick <chrishfitzpatrick@gmail.com>
2016-10-15 11:07:05 -0400

    back to http

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ function showPosition(position){
 var getWeather = function(mylat, mylong) {
     console.log("getWeather starting");
     gettingData = true;
-    var requestString = "https://api.openweathermap.org/data/2.5/weather?lat="
+    var requestString = "http://api.openweathermap.org/data/2.5/weather?lat="
                         + mylat + "&lon=" + mylong  
                         + "&APPID=68857f90d32a5dd13ead22ecc79f8af8";
     request = new XMLHttpRequest();
